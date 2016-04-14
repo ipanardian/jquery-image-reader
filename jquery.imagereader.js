9b5195bed11b5a18f53bc2071b96977cf0fec968
ipanardian <ipan.software@gmail.com>
2016-04-14 23:20:24 +0700

    Implement ES6

--- jquery.imagereader.js
+++ jquery.imagereader.js
@@ -8,14 +8,11 @@
 */
 
 (function (factory) {
-	// Start UMD
 	if (typeof define === 'function' && define.amd) {
-		// AMD (Register as an anonymous module)
 		define(['jquery'], factory);
 	} 
 	else if (typeof module === 'object' && module.exports) {
-		// Node/CommonJS
-		module.exports = function( root, jQuery ) {
+		module.exports = (root, jQuery) => {
 			if ( jQuery === undefined ) {
 				if ( typeof window !== 'undefined' ) {
 					jQuery = require('jquery');
@@ -25,46 +22,37 @@
 				}
 			}
 			factory(jQuery);
-
 			return jQuery;
 		};
 	} 
 	else {
-		// Browser globals
 		factory(jQuery);
 	}
-}(function ($) {
+}($ => {
 	$.fn.imageReader = function (options) {
-		// Default options
 		var defaults = {
 			destination: '#image-preview',
-			onload: function() {}
+			onload: () => {}
 		};
-		var settings = $.extend({}, defaults, options);
-		// Loop each object
-		return this.each(function() {
-			// Register onChange jQuery Event
-			$(this).on('change', function() {
+		var settings = Object.assign(defaults, options);
+
+		return this.each(function () {
+			$(this).on('change', () => {
 				var file;
 				var destination = $(settings.destination);
 				destination.html('');
-				// Loop for multple files
+
 				for(var x = 0, xlen = this.files.length; x < xlen; x++) {
 					file = this.files[x];
-					// Primitive Validate image
+					
 					if(file.type.indexOf('image') != -1) { 
-						// Create instance FileReader API
 						var reader = new FileReader();
-						// Triggered when reading operation is successfully completed
-						reader.onload = function(e) {
-							// Insert content into Image object
+						reader.onload = e => {
 							var img = new Image();
 							img.src = e.target.result;
 							destination.append(img);
-							// Call onload for callback
 							settings.onload.call(img);
 						};
-						// Starts reading the contents
 						reader.readAsDataURL(file);
 					}
 				}
