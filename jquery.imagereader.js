a99b8c9aac24f8e17a4117c83b84d0a78b5a18b1
ipanardian <ipan.software@gmail.com>
2016-04-15 10:49:13 +0700

    Implement Babel.js
    Tested on Chrome 49, Safari 9.1, Firefox 43. Working like a charm :)

--- jquery.imagereader.js
+++ jquery.imagereader.js
@@ -4,7 +4,7 @@
 *
 *  A jQuery plugin that previews image very fast without needing to upload to your server
 *  For details, see the web site: https://github.com/ipanardian/jquery-image-reader
-*  MIT License
+*  The MIT License
 */
 
 (function (factory) {
@@ -36,27 +36,34 @@
 		};
 		var settings = Object.assign(defaults, options);
 
-		return this.each(function () {
-			$(this).on('change', () => {
-				var file;
-				var destination = $(settings.destination);
-				destination.html('');
+		if ('FileReader' in window) {
 
-				for(var x = 0, xlen = this.files.length; x < xlen; x++) {
-					file = this.files[x];
-					
-					if(file.type.indexOf('image') != -1) { 
-						var reader = new FileReader();
-						reader.onload = e => {
-							var img = new Image();
-							img.src = e.target.result;
-							destination.append(img);
-							settings.onload.call(img);
-						};
-						reader.readAsDataURL(file);
+			return this.each(function () {
+				$(this).on('change', () => {
+					var file;
+					var destination = $(settings.destination);
+					destination.html('');
+
+					for(var x = 0, xlen = this.files.length; x < xlen; x++) {
+						file = this.files[x];
+						
+						if(file.type.indexOf('image') != -1) { 
+							
+							var reader = new FileReader();
+							reader.onload = e => {
+								var img = new Image();
+								img.src = e.target.result;
+								destination.append(img);
+								settings.onload.call(img);
+							};
+							reader.readAsDataURL(file);
+						}
 					}
-				}
+				});
 			});
-		});
+		}
+		else {
+			console.log('Your browser does not support FileReader API');
+		}
 	}
 }));
